13019320a8e5ffc2a151156daf2b6aaa6ec78c40
MateuszJanduraUszu <Mati4321@interia.pl>
2023-09-24 16:09:34 +0200

    Implement `shared_lock`, `lock_guard` and `shared_lock_guard`

---
+++ src/mjsync/srwlock.cpp
@@ -0,0 +1,46 @@
+// srwlock.cpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <mjsync/details/tinywin.hpp>
+#include <mjsync/srwlock.hpp>
+
+namespace mjsync {
+    shared_lock::shared_lock() noexcept : _Myimpl{0} {}
+
+    shared_lock::~shared_lock() noexcept {}
+
+    void shared_lock::lock() noexcept {
+        ::AcquireSRWLockExclusive(reinterpret_cast<SRWLOCK*>(&_Myimpl));
+    }
+
+    void shared_lock::lock_shared() noexcept {
+        ::AcquireSRWLockShared(reinterpret_cast<SRWLOCK*>(&_Myimpl));
+    }
+
+    void shared_lock::unlock() noexcept {
+#pragma warning(suppress : 26110) // C26110: lock must be held
+        ::ReleaseSRWLockExclusive(reinterpret_cast<SRWLOCK*>(&_Myimpl));
+    }
+
+    void shared_lock::unlock_shared() noexcept {
+        ::ReleaseSRWLockShared(reinterpret_cast<SRWLOCK*>(&_Myimpl));
+    }
+
+    lock_guard::lock_guard(shared_lock& _Lock) noexcept : _Mylock(_Lock) {
+        _Mylock.lock();
+    }
+
+    lock_guard::~lock_guard() noexcept {
+        _Mylock.unlock();
+    }
+
+    shared_lock_guard::shared_lock_guard(shared_lock& _Lock) noexcept : _Mylock(_Lock) {
+        _Mylock.lock_shared();
+    }
+
+    shared_lock_guard::~shared_lock_guard() noexcept {
+        _Mylock.unlock_shared();
+    }
+} // namespace mjsync
---
+++ src/mjsync/srwlock.hpp
@@ -0,0 +1,59 @@
+// srwlock.hpp
+
+// Copyright (c) Mateusz Jandura. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+#ifndef _MJSYNC_SRWLOCK_HPP_
+#define _MJSYNC_SRWLOCK_HPP_
+#include <mjsync/api.hpp>
+
+namespace mjsync {
+    class _MJSYNC_API shared_lock { // slim reader/writer lock
+    public:
+        shared_lock() noexcept;
+        ~shared_lock() noexcept;
+
+        shared_lock(const shared_lock&)            = delete;
+        shared_lock& operator=(const shared_lock&) = delete;
+
+        // acquires exclusive lock
+        void lock() noexcept;
+
+        // acquires shared lock
+        void lock_shared() noexcept;
+
+        // releases exclusive lock
+        void unlock() noexcept;
+
+        // releases shared lock
+        void unlock_shared() noexcept;
+
+    private:
+        struct _Impl { // copy of SRWLOCK structure
+            void* _Ptr;
+        };
+        
+        _Impl _Myimpl;
+    };
+
+    class _MJSYNC_API lock_guard { // automatically acquires and releases exclusive lock
+    public:
+        explicit lock_guard(shared_lock& _Lock) noexcept;
+        ~lock_guard() noexcept;
+
+    private:
+        shared_lock& _Mylock;
+    };
+
+    class _MJSYNC_API shared_lock_guard { // automatically acquires and releases shared lock
+    public:
+        explicit shared_lock_guard(shared_lock& _Lock) noexcept;
+        ~shared_lock_guard() noexcept;
+        
+    private:
+        shared_lock& _Mylock;
+    };
+} // namespace mjsync
+
+#endif // _MJSYNC_SRWLOCK_HPP_
